cc7d5eb403e0208b03ac6737bb1a1b4e7f1ab8f7
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2022-07-20 21:10:03 -0300

    feat: create endpoint POST /employees

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -3,6 +3,8 @@ package com.example.payroll;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +18,10 @@ class EmployeeController {
 	@GetMapping("/employees")
 	List<Employee> all() {
 		return repository.findAll();
+	}
+	
+	@PostMapping("/employees")
+	Employee newEmployee(@RequestBody Employee employee) {
+		return repository.save(employee);
 	}
 }
