bf20d7ad21bef4873ebdb62d9973bd07d14c5d25
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2022-07-20 21:28:02 -0300

    feat: create endpoint GET /employee/{id}

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.payroll;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,11 @@ class EmployeeController {
 	@PostMapping("/employees")
 	Employee newEmployee(@RequestBody Employee employee) {
 		return repository.save(employee);
+	}
+	
+	@GetMapping("/employee/{id}")
+	Employee one(@PathVariable Long id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new EmployeeNotFoundException(id));
 	}
 }
