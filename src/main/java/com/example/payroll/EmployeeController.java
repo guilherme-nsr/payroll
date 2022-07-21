be934120f41ef17fe047b7f2b40b8ad25b67104f
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2022-07-20 21:56:09 -0300

    feat: create endpoint DELETE /employees/{id}

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.payroll;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,5 +46,10 @@ class EmployeeController {
 					newEmployee.setId(id);
 					return repository.save(newEmployee);
 				});
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	void deleteEmployee(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
